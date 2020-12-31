28be9ad8edf989ab3edd777233bd4f5b85240bd8
Andrew-Chen-Wang <acwangpython@gmail.com>
2020-12-30 20:13:21 -0500

    Add

--- README.md
+++ README.md
@@ -2,9 +2,25 @@
 
 By: Andrew Chen Wang
 
-A template for me creating new Django tests, not for projects.
-Testing could mean performance testing, frontend
-configuring, etc. I dunno. Uses PostgreSQL.
+Edit (30 Dec 2020): I like can't figure out
+how connectby works since it's an Oracle thing,
+so I guess I'm sticking to my good pal recursive...
+
+Just testing counting of children from
+a single node. Need to compare speed with
+`connectby` and `with recursive` on Postgres.
+
+The creation of test data is using Python's
+PRNG and a happy message of a seed (using v2
+you can use a string to specify a seed value
+that's actually a string).
+
+---
+Usage:
+
+```shell
+python manage.py create
+```
 
 ---
 
--- public/management/commands/create.py
+++ public/management/commands/create.py
@@ -1,32 +1,87 @@
 from timeit import default_timer
 import os
+import random
+import time
 
-from django.conf import settings
+from django.db import connection
 from django.core.management.base import BaseCommand
+
+from public.models import Tree
 
 
 class Command(BaseCommand):
     help = (
-        "Generates test data of english dict"
+        "Generates test data of a tree hierarchy"
     )
     requires_migrations_checks = True
 
     def handle(self, *args, **options):
-        # Check if words_alpha.txt exists
-        path_to_dictionary = os.path.join(settings.BASE_DIR, "words_alpha.txt")
-        if not os.path.exists(path_to_dictionary):
-            import zipfile
-            try:
-                with zipfile.ZipFile(path_to_dictionary[:-3] + "zip", "r") as zip_ref:
-                    zip_ref.extractall(settings.BASE_DIR)
-            except FileNotFoundError as e:
-                raise FileNotFoundError(f"You must include the zip file of the English"
-                                        f" dictionary in the BASE_DIR, specified:\n{e}.")
+        if Tree.objects.count() > 0:
+            Tree.objects.all().delete()
+        random.seed("I wish I could just add something like 1984 to this"
+                    " seed but I can't because I'm an absolute moron."
+                    " But let the story continue on and on again", version=2)
+        # We're just gonna test a single parent node
+        initial = Tree.objects.create()
+        objs = [Tree.objects.first()]
+        start = default_timer()
+        t_end = time.time() + 60 * 2  # 2 minutes
+        while objs or time.time() < t_end:
+            # Decide if this has children
+            if bool(random.getrandbits(1)) or objs[0].parent is None:
+                # Now figure out how many
+                for _ in range(random.randint(0, 5)):
+                    objs.append(Tree.objects.create(parent=objs[0]))
+            objs.pop(0)
+        end = default_timer()
+        print(f"It took {end - start} seconds to create the objects.")
+        num_trees = Tree.objects.count()
+        print("Count:", num_trees)
+        # Create random trees to make sure the numbers are correct
+        Tree.objects.create()
+        _a = Tree.objects.create()
+        _b = Tree.objects.create(parent=_a)
+        Tree.objects.create(parent=_b)
+        Tree.objects.create(parent=_a)
 
-        def load_words() -> set:
-            """370099 words in the set"""
-            with open("words_alpha.txt") as file:
-                valid_words = set(file.read().split())
-            return valid_words
-        print("creating")
-        words = [x.lower() for x in load_words()]
+        print("Beginning test with connectby")
+        start = default_timer()
+        # Should be the correct number 732952
+        with connection.cursor() as c:
+            c.execute("""
+            """)
+            count = c.fetchone()[0]
+        assert count == num_trees
+        end = default_timer()
+        print(f"It took {end - start} seconds.")
+
+        print("Beginning test with WITH RECURSIVE")
+        start = default_timer()
+        with connection.cursor() as c:
+            c.execute("""
+            WITH RECURSIVE cte AS (
+               SELECT id, parent_id
+               FROM   public_tree
+               WHERE  id = %s
+            
+               UNION ALL
+               SELECT t.id, c.id
+               FROM public_tree t, cte c
+               WHERE t.parent_id = c.id
+            )
+            SELECT COUNT(*)
+            FROM cte
+            """, (initial.id,))
+            count = c.fetchone()[0]
+        assert count == num_trees
+        end = default_timer()
+        print(f"It took {end - start} seconds.")
+
+
+
+
+"""
+SELECT COUNT(*) FROM 
+    connectby('public_tree', 'id', 'parent_id', '0', 95128, '~')
+    AS t(id numeric, parent_id numeric, level int, branch text);
+"""
